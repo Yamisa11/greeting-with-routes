05a682748e35658513c6ed88fed9be70d4c5ae4c
Yamisa11 <majijayamisa@gmail.com>
2023-08-17 10:24:52 +0200

    error-handling

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import bodyParser from 'body-parser'
 import Greeting from './greeting.js';
 import GreetingDBLogic from './db/db.js';
 import DBJS from './database.js'
+import flash from "express-flash";
+import session from "express-session"
 
 const app = express()
 let database = GreetingDBLogic(DBJS)
@@ -16,18 +18,26 @@ app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
+app.use(session({
+    resave: false,
+    saveUninitialized: true,
+    secret: "Yamisa"
+
+}));
+app.use(flash())
+
 var  greetingFunction = Greeting()
-let error
+let errors
 let nameCounts
 let greetingMsg 
 
 app.get('/', (req,res) => {
-    
+    let errorMsg = req.flash("error")[0]
     res.render('index', {
         showGreet : greetingFunction.getInputName(),
-        theGreeting: error? "" : greetingMsg,
-        errorMessage: error,
-        counter : nameCounts
+        theGreeting: errorMsg? "" : greetingMsg,
+        errorMessage: errorMsg,
+        counter : errorMsg? "" : nameCounts
     })
 })
 
@@ -36,7 +46,8 @@ app.post('/greeting', (req,res) => {
     console.log(req.body.languageInput);
 //    greetingFunction.setInputName(req.body.theInputName)
 (greetingFunction.theGreeting(req.body.theInputName,req.body.languageInput,database))
-error = greetingFunction.errors(req.body.theInputName, req.body.languageInput);
+errors = greetingFunction.errors(req.body.theInputName, req.body.languageInput);
+req.flash('error', errors);
 greetingMsg = greetingFunction.getGreeting();
 console.log(greetingMsg);
 console.log(greetingFunction.errors(req.body.theInputName, req.body.languageInput));
